69070082297d0e404b573971f73fc227bcf93ec0
JohnCarlosGarcia <148402930+JohnC-Garcia@users.noreply.github.com>
2025-04-21 21:29:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model("C:/Users/User/Downloads/plant_classifier.hdf5")
+  model=tf.keras.models.load_model("plant_classifier.hdf5")
   return model
 model=load_model()
 st.write("""
@@ -16,7 +16,7 @@ from PIL import Image,ImageOps
 import numpy as np
 def import_and_predict(image_data,model):
     size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    image=ImageOps.fit(image_data,size,Image.LANCZOS)
     img=np.asarray(image)
     img_reshape=img[np.newaxis,...]
     prediction=model.predict(img_reshape)
